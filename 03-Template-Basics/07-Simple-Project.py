80638fa5a359b2ce22eddbe6a599fa4b97d601c4
Anand Vijay <anand.vijay@gmail.com>
2021-01-01 12:49:27 -0500

    more templating code

--- 03-Template-Basics/07-Simple-Project.py
+++ 03-Template-Basics/07-Simple-Project.py
@@ -1,9 +1,11 @@
 # Set up your imports and your flask app.
+from flask import Flask, render_template, request, url_for
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     # This home page should have the form.
-    pass
+    return render_template('07-index.html')
 
 
 # This page will be the page after the form
@@ -16,8 +18,28 @@ def report():
     # https://stackoverflow.com/questions/26515422/how-to-check-if-last-character-is-integer-in-raw-input
 
     # Return the information to the report page html.
-    pass
+
+    userName = request.args.get('userName')
+
+    if userNameTest(userName):
+        return render_template(('07-thank_you.html'), userName=userName)
+    else:
+        return render_template(('07-error.html'), userName=userName)
+
+#Helper function to test the validity of the user input
+def userNameTest(userName):
+    userNameFlag = True
+
+    if not any(c.islower() for c in userName):
+        userNameFlag = False
+
+    if not any(c.isupper() for c in userName):
+        userNameFlag = False
+
+    if not userName[-1].isdigit():
+        userNameFlag = False
+
+    return userNameFlag
 
 if __name__ == '__main__':
-    # Fill this in!
-    pass
+    app.run(debug=True)
